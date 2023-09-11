5f2784343e56db0075614cac1a942964c4a5fbf4
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-11 17:23:43 +0400

    Change example status to out of maintenance

--- VB/Form1.vb
+++
@@ -1,8 +0,0 @@
-Public Class Form1
-
-    Private Sub simpleButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles simpleButton1.Click
-        Dim frm As New Form2
-        EditorsHelper.SetStyleController(frm, styleController1)
-        frm.Show()
-    End Sub
-End Class
